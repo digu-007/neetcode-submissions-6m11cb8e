8a565ad7803f7aa9eadfdcb39f98445810200322
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-04-01 00:57:53 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& a) {
+        int n = a.size();
+        if (n == 1) return a[0];
+        int dp[n + 1][2] = {0};
+        dp[1][1] = a[0];
+        for (int i = 2; i <= n; ++i) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = max(dp[i - 1][0], dp[i - 2][1]) + a[i - 1];
+        }
+        return max(dp[n][0], dp[n][1]);
+    }
+};
