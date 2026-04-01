665f9fa2ccd31b2d7134fb288d063e7c2ad555a7
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-04-02 01:04:21 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size(), dp[n + 1];
+        if (s[0] == '0') return 0;
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            if (s[i - 1] == '0') {
+                if (s[i - 2] != '1' and s[i - 2] != '2') {
+                    return 0;
+                } else {
+                    dp[i] = dp[i - 2];
+                    continue;
+                }
+            }
+            dp[i] = dp[i - 1];
+            if (s[i - 2] == '1') {
+                if (s[i - 1] != '0') dp[i] += dp[i - 2];
+            }
+            if (s[i - 2] == '2') {
+                if (s[i - 1] > '0' and s[i - 1] < '7') dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
