07a0e54205938a135430871ca11b39dc025abc47
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-04-05 00:53:26 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool check(vector<int>& a, int cur, int h) {
+        for (auto &x: a) {
+            h -= (x + cur - 1) / cur;
+        }
+        return (h >= 0);
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1, hi = 1e9, mid, ans;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (check(piles, mid, h)) {
+                ans = mid, hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
