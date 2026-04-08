52f89b92cb9144d95a9a0eb7ec18d6dbd2026867
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-04-08 20:19:10 +0530

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& a) {
+        int n = a.size(), ans = 0, cur = 0, jump, mx, delta, next, i = 0;
+        while (i < n) {
+            if (i == n - 1) return ans;
+            ++ans;
+            jump = a[cur];
+            mx = 0;
+            while (i + 1 < n and jump > 0) {
+                ++i, --jump;
+                if (a[i] + cur - i >= mx) {
+                    mx = a[i] + cur - i;
+                    next = i;
+                }
+            }
+            cur = next;
+        }
+        return ans;
+    }
+};
