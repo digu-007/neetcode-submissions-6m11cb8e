d59a765a33c501a429f66918f33c18d7f21d5ec5
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-04-08 21:55:42 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size(), dp[amount + 1][n + 1] = {};
+        dp[0][0] = 1;
+        for (int j = 1; j <= n; ++j) {
+            for (int i = 0; i <= amount; ++i) {
+                dp[i][j] = dp[i][j - 1];
+                if (coins[j - 1] > i) continue;
+                dp[i][j] += dp[i - coins[j - 1]][j];
+            }
+        }
+        return dp[amount][n];
+    }
+};
