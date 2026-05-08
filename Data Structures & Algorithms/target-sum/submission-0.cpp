5c56fa0d6dde980718096a66e729a57366cdc2c9
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-08 22:49:26 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int limit;
+
+    int solve(vector<int>& a, vector<vector<int>>& dp, int idx, int cur, int target) {
+        if (idx < 0) return cur == target;
+        cout << idx << " " << cur << "\n";
+        if (dp[idx][cur + limit] != -1) return dp[idx][cur + limit];
+        return dp[idx][cur + limit] = solve(a, dp, idx - 1, cur + a[idx], target) + solve(a, dp, idx - 1, cur - a[idx], target);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        limit = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> dp(n, vector<int>(2 * limit + 1, -1));
+        return solve(nums, dp, n - 1, 0, target);
+    }
+};
