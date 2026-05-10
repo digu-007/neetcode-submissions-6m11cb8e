97dcca0aab156106c6dc5597647d81d34e156dfe
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-10 18:25:36 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        // A * * A * * A -> 3 + 2 * (3 - 1)
+        // A B * A B * A B -> 3 * 2 + 1 * (3 - 1)
+        int cnt[26] = {}, mx = 0, freq = 0;
+        for (auto &x: tasks) {
+            ++cnt[x - 'A'];
+            if (cnt[x - 'A'] > mx) {
+                mx = cnt[x - 'A'];
+                freq = 1;
+            } else if (cnt[x - 'A'] == mx) {
+                ++freq;
+            }
+        }
+        return max((int)tasks.size(), mx * freq + max((n - freq + 1), 0) * (mx - 1));
+    }
+};
