fb6724450c6586bc89d4b71492f96678ebfb730f
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-13 00:20:04 +0530

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> ans;
+
+    void dfs(vector<int>& a, vector<bool>& vis, vector<int>& cur) {
+        if (cur.size() == n) {
+            ans.push_back(cur);
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                cur.push_back(a[i]);
+                vis[i] = true;
+                dfs(a, vis, cur);
+                vis[i] = false;
+                cur.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        vector<bool> vis(n, false);
+        vector<int> cur;
+        dfs(nums, vis, cur);
+        return ans;
+    }
+};
