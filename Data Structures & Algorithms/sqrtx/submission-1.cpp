b657d3b6be4956475242223490855f52843f3c26
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-22 17:23:21 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 0, lo = 0, hi = 46340;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid * mid <= x) {
+                lo = mid + 1;
+                ans = max(ans, mid);
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
