e479bce44493018704b19d030b48beb40f7e7ecc
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-28 15:05:16 +0530

    Add: minimum-height-trees - submission-0

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> g;
+    vector<int> p;
+
+    pair<int, int> dfs(int node) {
+        int cur = node, max = 0;
+        for (auto &x: g[node]) {
+            if (p[x] == -1) {
+                p[x] = node;
+                pair<int, int> res = dfs(x);
+                if (res.second + 1 > max) {
+                    max = res.second + 1;
+                    cur = res.first;
+                }
+            }
+        }
+        return {cur, max};
+    }
+
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        g.resize(n);
+        p.resize(n, -1);
+        for (auto &x: edges) {
+            g[x[0]].push_back(x[1]);
+            g[x[1]].push_back(x[0]);
+        }
+        p[0] = 0;
+        int farthest = dfs(0).first;
+        fill(p.begin(), p.end(), -1);
+        p[farthest] = 0;
+        int other = dfs(farthest).first;
+        cout << farthest << " " << other << "\n";
+        vector<int> res = {other};
+        while (other != farthest) {
+            other = p[other];
+            res.push_back(other);
+        }
+        for (auto &x: res) {
+            cout << x << " ";
+        }
+        int sz = res.size();
+        if (sz & 1) {
+            return {res[sz / 2]};
+        }
+        return {res[sz / 2], res[sz / 2 - 1]};
+    }
+};
