2e62377ac3e11ed4ebed91feaac367429d0479bf
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-05-31 16:49:32 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        // if (mp.count(key)) {
+            mp[key].push_back({timestamp, value});
+        // } else {
+        //     mp[key] = {(timestamp, value)};
+        // }
+    }
+    
+    string get(string key, int timestamp) {
+        if (!mp.count(key)) {
+            return "";
+        }
+        auto &cur = mp[key];
+        pair<int, string> p = {timestamp + 1, ""};
+        auto itr = lower_bound(cur.begin(), cur.end(), p);
+        if (itr == cur.begin()) {
+            return "";
+        }
+        --itr;
+        return itr -> second;
+    }
+};
