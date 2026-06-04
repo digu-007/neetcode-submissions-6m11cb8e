2f51d97a6fb759042f1f3373624a7382681b0017
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-06-05 00:10:21 +0530

    Add: minimum-array-end - submission-3

---
+++ Data Structures & Algorithms/minimum-array-end/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        // 10110: 10110 10111
+        // 10011: 10 11
+        // 10: 11
+        // 11: 10011
+        --n;
+        bitset<40> bits(n);
+        long long ans = x, i = 0, ctr = 0;
+        while (ctr < 40) {
+            if (!(x & 1)) {
+                bool bit = bits[ctr++];
+                if (bit) ans += ((long long)1 << i);
+
+            }
+            x >>= 1;
+            ++i;
+        }
+        return ans;
+    }
+};
