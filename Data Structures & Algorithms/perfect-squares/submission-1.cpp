901bcb2a580590fefc9222a7fc2d1e3a17370984
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-06-05 23:05:20 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        queue<int> q;
+        vector<int> d(n + 1, 0);
+        q.push(0);
+        while (q.size()) {
+            int cur = q.front();
+            q.pop();
+            for (int i = 1; i * i <= n - cur; ++i) {
+                int num = i * i + cur;
+                if (!d[num]) {
+                    q.push(num);
+                    d[num] = 1 + d[cur];
+                }
+            }
+        }
+        return d[n];
+    }
+};
