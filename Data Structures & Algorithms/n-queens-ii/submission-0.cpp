b21c823b7f7c033bd6d9925858c06e44d9e09842
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-06-06 02:47:08 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int sz;
+    vector<vector<string>> ans;
+
+    void dfs(int queens, vector<int>& j, vector<string>& grid) {
+        if (queens == sz) {
+            ans.push_back(grid);
+            return;
+        }
+        vector<bool> possible(sz, true);
+        for (int i = 0; i < j.size(); ++i) {
+            possible[j[i]] = false;
+            int diff = queens - i;
+            if (j[i] + diff < sz) possible[j[i] + diff] = false;
+            if (j[i] - diff >= 0) possible[j[i] - diff] = false;
+        }
+        for (int i = 0; i < sz; ++i) {
+            if (possible[i]) {
+                j.push_back(i);
+                grid[queens][i] = 'Q';
+                dfs(queens + 1, j, grid);
+                grid[queens][i] = '.';
+                j.pop_back();
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        sz = n;
+        vector<string> grid(sz, "");
+        for (int i = 0; i < sz; ++i) {
+            for (int j = 0; j < sz; ++j) {
+                grid[i] += '.';
+            }
+        }
+        vector<int> j;
+        dfs(0, j, grid);
+        return (int)ans.size();
+    }
+};
