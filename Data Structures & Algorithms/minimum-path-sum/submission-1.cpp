fcfa022d151a843667b7ab7ebf12d01934dbe342
Digvijay Janartha <31317843+digu-007@users.noreply.github.com>
2026-06-08 15:17:35 +0530

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& a) {
+        int n = a.size(), m = a[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1e6));
+        dp[n][m - 1] = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                dp[i][j] = min(dp[i + 1][j] + a[i][j], dp[i][j + 1] + a[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+};
